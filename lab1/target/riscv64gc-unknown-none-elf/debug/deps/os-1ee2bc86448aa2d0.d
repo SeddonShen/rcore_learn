c0b3f7d92b4c833420dd755af641f63e41019757
SeddonShen <283481855@qq.com>
2021-09-07 16:00:32 +0800

    Hello

--- lab1/target/riscv64gc-unknown-none-elf/debug/deps/os-1ee2bc86448aa2d0.d
+++
@@ -1,9 +0,0 @@
-/root/lab1/rcore_learn/lab1/target/riscv64gc-unknown-none-elf/debug/deps/os-1ee2bc86448aa2d0: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
-
-/root/lab1/rcore_learn/lab1/target/riscv64gc-unknown-none-elf/debug/deps/os-1ee2bc86448aa2d0.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
-
-src/main.rs:
-src/console.rs:
-src/lang_items.rs:
-src/sbi.rs:
-src/entry.asm:
